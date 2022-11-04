33d26d6fffb224fdf3d28616ac39e9d3152156f2
madu <mr.madu@ya.ru>
2022-11-04 15:36:50 +0300

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+git switch main
+git checkout --orphan temp_branch
+git add -A
+git commit -am "the first commit"
+git branch -D gh-pages
+git push origin --delete gh-pages
+git branch -m gh-pages
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+npm run build
+
+
+# git init
+git add dist
+git commit -m 'add dist subtree and deploy'
+
+git subtree push --prefix dist origin gh-pages
+# git push origin `git subtree split --prefix dist main`:gh-pages --force
+
+git switch -f main
+
+# cd -
